8c4d761c1a082afe3f6018af526f8f85c022960f
Pedro Gutierres <pedro@imperiumsolucoes.com.br>
2024-10-02 17:05:29 -0300

    Adicionado component quill para conteudo de texto dos posts

---
+++ src/Blog.Web/Helpers/CustomHtmlHelpers.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Blog.Web.Helpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static string RemoverTagsHtml(string input)
+        {
+            return Regex.Replace(input, "<.*?>", string.Empty);
+        }
+    }
+}
--- src/Blog.Web/ViewModels/Posts/PostResumidoViewModel.cs
+++ src/Blog.Web/ViewModels/Posts/PostResumidoViewModel.cs
@@ -1,10 +1,13 @@
-﻿namespace Blog.Web.ViewModels.Posts
+﻿using Blog.Web.Helpers;
+
+namespace Blog.Web.ViewModels.Posts
 {
     public class PostResumidoViewModel
     {
         public Guid Id { get; set; }
         public string Titulo { get; set; }
         public string ConteudoResumido { get; set; }
+        public string ConteudoResumidoHtml => CustomHtmlHelpers.RemoverTagsHtml(ConteudoResumido);
         public DateTime DataHoraCriacao { get; set; }
         public Guid AutorId { get; set; }
         public string AutorNomeCompleto { get; set; }
